7b264900f723e85922c044bdd720c9e0f59d16fa
Justin Bass <justinbass6036@gmail.com>
2022-11-14 20:40:14 -0500

    commit message

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   return 'hello'
 });
+
+
+document.getElementById('title').innerText = "RITAS"
+document.getElementById('venue').src= "https://images.squarespace-cdn.com/content/v1/5ded6e17e5b0744826c470fc/1575856661048-D3GR7XK0ZHVJ8CRV5GQE/_MG_2705.jpg?format=1500w"
 
 
 function renderMargarita(menu) {
@@ -9,26 +12,12 @@ function renderMargarita(menu) {
     card.className = 'card'
     card.id = 'cardId'
 
+    let h1 = document.createElement('h1')
+    h1.className = 'drinkTitles'
+    h1.innerText = menu.name
+
 
         document.querySelector('#menu').appendChild(card)
 }
 margaritaData()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
